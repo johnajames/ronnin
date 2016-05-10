948b48044dc2a4b2e0656c1df7afc4ad159d6c5a
johnajames <john@20first.io>
2016-05-09 20:01:02 -0500

    blah

--- server.rb
+++ server.rb
@@ -3,12 +3,6 @@ require 'json'
 
 post '/payload' do
   push = JSON.parse(request.body.read)
+  byebug
   puts "I got some JSON: #{push.inspect}"
-end
-
-if push.key?('ref')
-  if push.ref.include? 'master'
-    puts '_____________________'
-    puts 'this is  master push'
-  end
 end
