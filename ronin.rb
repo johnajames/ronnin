387b91677fe7e2f97d1986c03f0ec5395127d990
johnajames <john@20first.io>
2016-05-18 15:01:45 -0500

    added

--- ronin.rb
+++ ronin.rb
@@ -1,11 +1,22 @@
 class Ronin
+  def initialize(username, password)
+    @username = username
+    @password = password
+  end
+
   def push(repository)
-    # we are going to merge the development branch into master
-    `git checkout master`
-    `git merge development`
-    # if conflicts send message script
-    # how to handle if conflicts arise
-    `git push origin master`
+    `git push`
+  end
+
+  def pull(repository)
+    `git pull`
+  end
+
+  def send_message
+  end
+
+  def run_test
+    `python functional_test.py`
   end
 
 
