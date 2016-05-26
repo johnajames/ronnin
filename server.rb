f0397c23aea0fdd1dec26d7ad70e457ee3ee2919
johnajames <john@20first.io>
2016-05-26 18:49:15 -0500

    updated server

--- server.rb
+++ server.rb
@@ -22,15 +22,15 @@ post '/payload' do
   current_tag = `git tag`
   push = JSON.parse(request.body.read)
   # lets gather the data we want and store them in variables
-    # every event is monitored so all you have to do is change the tag
-  pusher = push["pusher"]["name"]
-  new_tag = push["tag_name"]
+  # every event is monitored so all you have to do is change the tag
+  # pusher = push["pusher"]["name"]
+  new_tag = push['tag_name']
 
   # check our criteria
   if current_tag != new_tag
     # move into directory of the project
     begin
-      Dir.chidir("io") do
+      Dir.chidir('../greenzone') do
         process_id = `lsof -wni tcp:8000`
         `kill -9 process_id`
         `git pull origin master`
@@ -43,16 +43,16 @@ post '/payload' do
         `uwsgi_python27 --reload io.pid`
       end
     rescue
-      puts "could not access directory"
+      puts 'could not access directory'
     end
   end
 
-    Mail.deliver do
-      # a daemon e-mail account
-      from 'test@wm.com'
-      to 'jbenne@wm.com'
-      subject "updated are currently staged and ready to be tested"
-      body "The following have been modified #{files}"
-    end
-  puts "I got some JSON: #{push.inspect}"
+    # Mail.deliver do
+    #   # a daemon e-mail account
+    #   from 'test@wm.com'
+    #   to 'jbenne@wm.com'
+    #   subject "updated are currently staged and ready to be tested"
+    #   body "The following have been modified #{files}"
+    # end
+    puts "I got some JSON: #{push.inspect}"
 end
