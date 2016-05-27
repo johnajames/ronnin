38a6f5d333fb034e983c67476d556d43f5fd27bd
johnajames <john@20first.io>
2016-05-26 19:04:00 -0500

    commented out code for testing

--- server.rb
+++ server.rb
@@ -19,33 +19,33 @@ require 'mail'
 # executed the git commands
 
 post '/payload' do
-  current_tag = `git tag`
-  push = JSON.parse(request.body.read)
-  # lets gather the data we want and store them in variables
-  # every event is monitored so all you have to do is change the tag
-  # pusher = push["pusher"]["name"]
-  new_tag = push['tag_name']
-
-  # check our criteria
-  if current_tag != new_tag
-    # move into directory of the project
-    begin
-      Dir.chidir('../greenzone') do
-        process_id = `lsof -wni tcp:8000`
-        `kill -9 process_id`
-        `git pull origin master`
-        `echo username`
-        `yes`
-        `echo password`
-        `yes`
-        `/manage collectstatic`
-        `./manage migrate`
-        `uwsgi_python27 --reload io.pid`
-      end
-    rescue
-      puts 'could not access directory'
-    end
-  end
+  # current_tag = `git tag`
+  # push = JSON.parse(request.body.read)
+  # # lets gather the data we want and store them in variables
+  # # every event is monitored so all you have to do is change the tag
+  # # pusher = push["pusher"]["name"]
+  # new_tag = push['tag_name']
+  #
+  # # check our criteria
+  # if current_tag != new_tag
+  #   # move into directory of the project
+  #   begin
+  #     Dir.chidir('../greenzone') do
+  #       process_id = `lsof -wni tcp:8000`
+  #       `kill -9 process_id`
+  #       `git pull origin master`
+  #       `echo username`
+  #       `yes`
+  #       `echo password`
+  #       `yes`
+  #       `/manage collectstatic`
+  #       `./manage migrate`
+  #       `uwsgi_python27 --reload io.pid`
+  #     end
+  #   rescue
+  #     puts 'could not access directory'
+  #   end
+  # end
 
     # Mail.deliver do
     #   # a daemon e-mail account
